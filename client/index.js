153d847d3f3160f379ad0a78a86c942f175e3f5b
Stanislav Sysoiev <sysstas@gmail.com>
2017-08-08 13:26:18 +0300

    connect to MongoDB

--- client/index.js
+++ client/index.js
@@ -1,9 +1,36 @@
 "use strict";
 
 
-  angular.module('dateExample', ['ngAnimate'])
-    .controller('DateController', ['$scope', function($scope) {
-       $scope.example = {
-         value: new Date(2013, 9, 22)
-       };
-     }]);
+angular.module('cheklistKeeper', ['ngAnimate'])
+
+.controller('DateController', ['$scope', function($scope) {
+   $scope.example = {
+     value: new Date(2013, 9, 22)
+   };
+ }])
+
+.controller('auditsCtrl', function($scope, $http){
+
+	$http.get('/auditors').then((response)=>{
+		$scope.auditors = response.data;		
+		console.log(response.status);
+	});
+
+/*$scope.auditors = [
+	{
+		name: "Iron Man",
+		audits: [{month: "june", amount: 3}]
+	},
+	{
+		name: "Thor",
+		audits: [{month: "june", amount: 2}]
+	},
+	{
+		name: "Spider Men",
+		audits: [{month: "june", amount: 1}]
+	}
+];*/
+	
+		
+	
+});
--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ let express = require('express');
 let app = express();
 let MongoClient = require('mongodb').MongoClient;
 
-/*let arr = [];
+let arr = [];
+	
 MongoClient.connect('mongodb://localhost:27017/auditors', (error, db) => {
 	console.log('Connected to Mongo');
-
 	
 	let querry = {};
 	let cursor = db.collection('auditors').find(querry);
@@ -18,16 +18,18 @@ MongoClient.connect('mongodb://localhost:27017/auditors', (error, db) => {
 
 	
 });
-*/
+
 app.use( '/', express.static(__dirname + "/../client") );
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 
 
-/*
+
 app.get('/auditors', (req,res)=>{
 	console.log(arr);
 	res.json(arr);
-});'172.21.164.171'
-*/
+});
+
 let serverPort = 3000;
 app.listen(serverPort, ()=> console.log('Server is running on ' + serverPort + ' port'));
+
+/*'172.21.164.171'*/
